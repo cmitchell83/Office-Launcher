8a29b116504a6c50f9ffc336ad41052f2b1e35bc
cmitchell83 <31594657+cmitchell83@users.noreply.github.com>
2017-09-03 14:04:28 -0400

    Add files via upload

---
+++ Source/Microsoft Office 2013 Launcher/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Microsoft Office Launcher")> 
+<Assembly: AssemblyDescription("Microsoft Office Launcher App")> 
+<Assembly: AssemblyCompany("Morbix")> 
+<Assembly: AssemblyProduct("Microsoft Office Launcher")> 
+<Assembly: AssemblyCopyright("Copyright ©  2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0c2b2e93-043c-4fdf-bc1b-0af54d56d60e")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.2.2.*")> 
+<Assembly: AssemblyFileVersion("1.2.2.*")> 
